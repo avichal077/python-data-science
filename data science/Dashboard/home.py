0342fe7b0bdb446d0b354ad02eb462a6cdc8b037
Avichal Saxena <avichalsaxena371@gmail.com>
2025-01-24 18:42:30 +0530

    dashboard

--- data science/Dashboard/home.py
+++ data science/Dashboard/home.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title("Dashboard")
+
+st.image("assets\images.jpeg")
--- data science/Dashboard/pages/dashboard.py
+++ data science/Dashboard/pages/dashboard.py
@@ -1,3 +1,12 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+import seaborn as sns 
+# load the titanic dataset 
+titanic = sns.load_dataset("titanic")
+st.title("titanic dataset dashboard")
+st.sidebar.header("filter options")
 
-st.title("Dashboard")
+st.dataframe(titanic)
