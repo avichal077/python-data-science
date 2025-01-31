3a1cfb48bd04ab17970d6eebef4eafdd96649f23
Avichal Saxena <avichalsaxena371@gmail.com>
2025-01-31 19:18:53 +0530

    dashboard

--- data science/Dashboard/home.py
+++ data science/Dashboard/home.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title("Dashboard")
+st.markdown('<h1 style="color: white; text-align: center; border: 5px solid black; background-color: darkgreen;" > Titanic Dashboard </h1>', unsafe_allow_html=True)
 
 st.image("assets\images.jpeg")
